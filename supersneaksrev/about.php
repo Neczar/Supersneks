cf017d7cda8508959e6f5bbdbcf61f90c5e03be3
Neczar <balagulan.neczar@gmail.com>
2025-01-17 21:21:27 +0800

    commit initial

--- supersneaksrev/about.php
+++ supersneaksrev/about.php
@@ -8,7 +8,7 @@ $customer_id = $_SESSION['customer_id'];
 
 if (!isset($customer_id)) {
    header('location:login.php');
-   exit; // Exit the script to prevent further execution
+   exit; // Exit the script to prevent further executions 
 }
 
 
@@ -22,7 +22,7 @@ if (!isset($customer_id)) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>about</title>
+   <title>abouts</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
